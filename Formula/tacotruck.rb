08273b95dfeadf5b70d2f4724498fd91ab025198
Chetan <chetankharel7@gmail.com>
2025-10-24 13:59:01 +0545

    fix: corrupted sha values (#5)

--- Formula/tacotruck.rb
+++ Formula/tacotruck.rb
@@ -1,34 +1,30 @@
 class Tacotruck < Formula
   desc "Test/QA data pipeline by TestFiesta"
   homepage "https://github.com/testfiesta/tacotruck"
-  version "1.0.0-beta.27"
+  version "1.0.0-beta.26"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-darwin-x64"
-      sha256 "[0;32m[INFO][0m Downloading https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-darwin-x64
-9c56abb477801ac3f7e50bfb4f796d847fbccb89dd99c0f3cdd13c64503fad27"
+      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.26/tacotruck-1.0.0-beta.26-darwin-x64"
+      sha256 "0f723ce55ab9881dbfce02683d7e449f27409e7bdb1e83d5183bfc593f5803a5"
     end
 
     on_arm do
-      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-darwin-arm64"
-      sha256 "[0;32m[INFO][0m Downloading https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-darwin-arm64
-fd7a7d7b21cdc1ea4b45ec2e403571367e70032170d54e62278577ae293e207b"
+      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.26/tacotruck-1.0.0-beta.26-darwin-arm64"
+      sha256 "5a1364fd3c2b59a83aed5f61ac4c8a799d39a2150a9ab37b131922e7daa58fcc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-linux-x64"
-      sha256 "[0;32m[INFO][0m Downloading https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-linux-x64
-5729a1ca7ee667cd7b6bdbaa9212b622589004effd0ca90451fd739bfb38f236"
+      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.26/tacotruck-1.0.0-beta.26-linux-x64"
+      sha256 "4f52c0af0675e5aa228de6a0420003d45780979777c5863ad85b06c79eb147c9"
     end
 
     on_arm do
-      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-linux-arm64"
-      sha256 "[0;32m[INFO][0m Downloading https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.27/tacotruck-1.0.0-beta.27-linux-arm64
-addeaf54070b455656d701e1c5d8b805f08cbc452dc709b6da56aaf1cd2420bc"
+      url "https://github.com/testfiesta/tacotruck/releases/download/v1.0.0-beta.26/tacotruck-1.0.0-beta.26-linux-arm64"
+      sha256 "b76e754206f9c29a1cfe3eac368655760b608a5fb64afe7b361ba44504eca6da"
     end
   end
 
